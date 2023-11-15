7f613463597acdf073524262c0b26fc6125b7ee9
MQuaresma <miguelquaresma97@gmail.com>
2023-11-15 20:13:05 +1100

    Support pextrq instructions

--- src/instructions.c
+++ src/instructions.c
@@ -25,13 +25,13 @@ const struct opd_format_table OPD_FORMAT_TABLE[] = {
                         {n,   {'\0'}}, {n,   "i"},    
                         // first operand is a memory reference
                         {m,   "m"},   {mi,  "mi"},  {mr,  "mr"}, {mx, "mx"},
-                        {mri, "mri"}, {mrr, "mrr"}, {mv,   "mv"},    
-                        {my,  "my"}, 
+                        {mri, "mri"}, {mrr, "mrr"}, {mv,   "mv"}, {mvi, "mvi"},
+                        {my,  "my"},
                         // first operand is a register
                         {r,   "r"},   {ri,  "ri"},  {rm,  "rm"}, {rx, "rx"},
                         {rmi, "rmi"}, {rmr, "rmr"}, {rr,  "rr"},
                         {rri, "rri"}, {rrm, "rrm"}, {rrr, "rrr"},
-                        {rv,  "rv"},
+                        {rv,  "rv"}, {rvi, "rvi"},
 												// first operand is a mmx register
 												{xr, "xr"}, {xm, "xm"},
                         // first operand is a xmm register 
@@ -200,7 +200,8 @@ const struct instr_table INSTR_TABLE[] = {
     {{'\0'},        pand,        {rm, rr},   RM,  VECTOR,         NA,  NA,  4,  {REX,  0x0f, 0xdb, REG}},
     {"pandn",       pandn,       {vm, vv},   RM,  VECTOR,         NA,  NA,  5,  {0x66, REX, 0x0f, 0xdf, REG}},
     {{'\0'},        pandn,       {rm, rr},   RM,  VECTOR,         NA,  NA,  4,  {REX,  0x0f, 0xdf, REG}},
-    {"pinsrq",      pinsrq,      {vmi,vri},  RM,  VECTOR,         NA,  NA,  6,  {0x66, REX, 0x0f, 0x3a, 0x22, REG}},
+    {"pextrq",      pextrq,      {rvi, mvi}, MR,  VECTOR,         NA,  NA,  6,  {0x66, REX, 0x0f, 0x3a, 0x16, REG}},
+    {"pinsrq",      pinsrq,      {vmi, vri}, RM,  VECTOR,         NA,  NA,  6,  {0x66, REX, 0x0f, 0x3a, 0x22, REG}},
     {"pmulhrsw",    pmulhrsw,    {vm, vv},   RM,  VECTOR,         NA,  NA,  6,  {0x66, REX, 0x0f, 0x38, 0x0b, REG}},
     {{'\0'},        pmulhrsw,    {rm, rr},   RM,  VECTOR,         NA,  NA,  5,  {REX, 0x0f, 0x38, 0x0b, REG}},
     {"pmulhuw",     pmulhuw,     {vm, vv},   RM,  VECTOR,         NA,  NA,  5,  {0x66, REX, 0x0f, 0xe4, REG}},
